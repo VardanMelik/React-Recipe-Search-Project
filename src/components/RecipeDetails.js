dac390d057afa6713fd5ece752762e948185f3a1
eyemotion <eyemotiongamestudio@gmail.com>
2019-10-01 21:55:50 +0400

    update

---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class RecipeDetails extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        Hello From Recipe Details
+      </React.Fragment>
+    )
+  }
+}
