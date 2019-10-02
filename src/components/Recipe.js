a1c0fe0df69360a57dcd80b1bb4b4887eb302d97
eyemotion <eyemotiongamestudio@gmail.com>
2019-10-02 20:51:22 +0400

    update

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react'
 
 export default class Recipe extends Component {
   render() {
-    console.log(this.props);
+    const {
+      image_url,
+      title,
+      source_url,
+      publisher,
+      recipe_id
+    } = this.props.recipe
+
+    //console.log(this.props.recipe);
     return (
       <React.Fragment>
-        <h1>Hello From Recipe</h1>
+        <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+          <div className="card">
+            <img className="img-card-top" style={{height: "14rem"}} src={image_url} alt="image" />
+            <div className="card-body text-capitalize">
+            <h6>{title}</h6>
+            <h6 className="text-warning text-slanted">
+              provided by {publisher}
+            </h6>
+            </div>
+          </div>
+        </div>
       </React.Fragment>
 
     )
